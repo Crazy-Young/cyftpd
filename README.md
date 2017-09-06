16af19fadbbf4507e1e0ef4e33097ea36a431009
Crazy-Young <wang960909@gmail.com>
2017-09-06 08:32:53 +0800

    temp test

--- README.md
+++ README.md
@@ -6,3 +6,15 @@ Away from VSFTPD...Will be a better choice...
 深受vsftpd的配置痛苦,并且必须绑定访问地址的脑残设定...
 决定设计一个更简单的ftpServer..
 
+
+## 环境搭建..
+我们需要一下的这些包
+
+1. 程序本身(鬼都知道).
+2. python3的版本
+	* 不清楚的可以使用`python -V 去看一下版本`
+	* 如果是2.x版本的话看看有没有python3这个命令
+3. pyftpdlib的库-使用pip安装即可
+4. 强烈建议开启的ssl..所以安装.默认如果没有配置会自动给你配置.PyOpenSSL的库-使用pip安装即可
+5. 如果用户名等信息存储在mysql里面.请安装pymysqld库
+
--- src/Main.py
+++ src/Main.py
@@ -3,6 +3,32 @@ Created on 2017年9月3日
 
 @author: wang9
 '''
+from pyftpdlib.authorizers import DummyAuthorizer
+from pyftpdlib.handlers import FTPHandler
+from pyftpdlib.servers import FTPServer
+
+
+
+def main():
+    pass
+
+
+def test():
+    # 实例化虚拟用户，这是FTP验证首要条件
+    authorizer = DummyAuthorizer()
+    # 添加用户权限和路径，括号内的参数是(用户名， 密码， 用户目录， 权限)
+    authorizer.add_user('user', '12345', 'F:\TOOL', perm='elradfmw')
+
+
+    # 初始化ftp句柄
+    handler = FTPHandler
+    handler.authorizer = authorizer
+    
+    handler.passive_ports = range(2000, 2333)
+    # 监听ip 和 端口,因为linux里非root用户无法使用21端口，所以我使用了2121端口
+    server = FTPServer(('0.0.0.0', 21), handler)
+    # 开始服务
+    server.serve_forever()
 
 if __name__ == '__main__':
-    pass
+    test()
